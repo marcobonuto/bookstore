818a4269532754ffbfb0caf5969b12b294a2278f
marcobonuto <marcobonuto@yahoo.com>
2016-03-17 22:38:12 -0300

    Anotações gerais da aula do dia 16-03

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
+
+# ------------------------------------------------
+# -   Arquivo para alimentar o banco de dados.   -
+# ------------------------------------------------
+
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
